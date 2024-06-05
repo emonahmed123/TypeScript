1f6ff19522c05cfa36d972816a344190b6804100
Emon Ahmed <imonshomon@gmail.com>
2024-06-05 08:10:20 +0600

    first commit

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const course = "Next Level";
+console.log(course);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const course: string = "Next Level";
+console.log(course);
+//  primitive Types
+
+// stirng
+// number
+// boolean
+// null
+// undifined
